9bd915f3b77e149180537a2311ba311f1dff4270
dmitrymail_k <dmitrymail_k@mail.ru>
2020-12-05 02:09:09 +0400

    rm

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
-// ======================================================================
-//  main.cpp
-// ======================================================================
-//                   This file is a part of the book
-//             "Qt 5.10 Professional programming with C++"
-//                       http://qt-book.com
-// ======================================================================
-//  Copyright (c) 2017 by Max Schlee
-// ======================================================================
 
 #include <QtWidgets>
 #include <bits/stdc++.h>
-// ----------------------------------------------------------------------
 
-
-
-
-
-
-int main (int argc, char** argv)
+int main(int argc, char **argv)
 {
-
 
     QApplication app(argc, argv);
 
@@ -28,34 +12,34 @@ int main (int argc, char** argv)
     // 2. Дана строка. Вывести строку, содержащую те же символы, но расположенные в обратном порядке.
 
     QWidget mainWidget;
-    QBoxLayout* mainField = new QBoxLayout(QBoxLayout::BottomToTop);
+    QBoxLayout *mainField = new QBoxLayout(QBoxLayout::BottomToTop);
 
-    QTextEdit* textEdit = new QTextEdit();
+    QTextEdit *textEdit = new QTextEdit();
     textEdit->setReadOnly(true);
 
-    QLineEdit* button = new QLineEdit;
-    QPushButton* pcmdNormal = new QPushButton("&Push me down!");
+    QLineEdit *button = new QLineEdit;
+    QPushButton *pcmdNormal = new QPushButton("&Push me down!");
 
     // event listener on button
-     mainWidget.connect(pcmdNormal, &QPushButton::pressed,
-                        [textEdit, button](){
-         QString inside = button->text();
+    mainWidget.connect(pcmdNormal, &QPushButton::pressed,
+                       [textEdit, button]() {
+                           QString inside = button->text();
 
-         button->setText("");
-         if(inside.size() > 0){
-             // TASK 1
-             textEdit->append("Your input: " + inside);
-             QString strUpper = inside.toUpper();
-             QString strLower = inside.toLower();
-             textEdit->append("Lower: " + strLower);
-             textEdit->append("Upper: " + strUpper);
-             // TASK 2
-             QString strReverse = inside;
-             std::reverse(strReverse.begin(), strReverse.end());
-             textEdit->append("Reversed: " + strReverse);
-         }
-
-     });
+                           button->setText("");
+                           if (inside.size() > 0)
+                           {
+                               // TASK 1
+                               textEdit->append("Your input: " + inside);
+                               QString strUpper = inside.toUpper();
+                               QString strLower = inside.toLower();
+                               textEdit->append("Lower: " + strLower);
+                               textEdit->append("Upper: " + strUpper);
+                               // TASK 2
+                               QString strReverse = inside;
+                               std::reverse(strReverse.begin(), strReverse.end());
+                               textEdit->append("Reversed: " + strReverse);
+                           }
+                       });
 
     // add widgets
     mainField->addWidget(textEdit);
@@ -67,10 +51,6 @@ int main (int argc, char** argv)
     mainWidget.resize(250, 250);
     mainWidget.setLayout(mainField);
     mainWidget.show();
-
-
-
-
 
     return app.exec();
 }
